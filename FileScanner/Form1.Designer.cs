6845d5b067bb973776c849b1a3648b914aeecb02
PorterYen8823 <porteryglue@outlook.com>
2023-03-28 20:34:18 +0800

    加入專案檔案。

---
+++ FileScanner/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FileScanner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(116, 53);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(156, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(291, 53);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(156, 38);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "btnPause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(469, 53);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(156, 38);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "btnStop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(116, 172);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(112, 25);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "statusLabel";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 316);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ FileScanner/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileScanner
+{
+    public partial class MainForm : Form
+    {
+        private bool isRunning = false;
+        private bool isPaused = false;
+        private string sourceDir;
+        private string destDir;
+        private int interval;
+        private Dictionary<string, DateTime> copiedFiles = new Dictionary<string, DateTime>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadSetup();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                isPaused = false;
+                btnStart.Enabled = false;
+                btnPause.Enabled = true;
+                btnStop.Enabled = true;
+                StartScanner();
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (isRunning && !isPaused)
+            {
+                isPaused = true;
+                btnPause.Text = "Resume";
+            }
+            else if (isRunning && isPaused)
+            {
+                isPaused = false;
+                btnPause.Text = "Pause";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                isRunning = false;
+                btnStart.Enabled = true;
+                btnPause.Enabled = false;
+                btnPause.Text = "Pause";
+                btnStop.Enabled = false;
+            }
+        }
+
+        private void StartScanner()
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback((state) =>
+            {
+                while (isRunning)
+                {
+                    if (!isPaused)
+                    {
+                        string[] files = Directory.GetFiles(sourceDir, "*.raw");
+                        foreach (string file in files)
+                        {
+                            string destFile = Path.Combine(destDir, Path.GetFileName(file));
+                            if (copiedFiles.ContainsKey(destFile) && DateTime.Now - copiedFiles[destFile] < TimeSpan.FromMinutes(2))
+                            {
+                                continue;
+                            }
+                            int count = 1;
+                            string newDestFile = destFile;
+                            while (File.Exists(newDestFile))
+                            {
+                                newDestFile = Path.Combine(destDir, Path.GetFileNameWithoutExtension(destFile) + "-" + count + Path.GetExtension(destFile));
+                                count++;
+                            }
+                            try
+                            {
+                                File.Copy(file, newDestFile);
+                            }
+                            
+                            catch
+                            {
+
+                            }
+                            copiedFiles[newDestFile] = DateTime.Now;
+                        }
+                    }
+                    Thread.Sleep(interval);
+                }
+            }));
+        }
+
+        private void LoadSetup()
+        {
+            try
+            {
+                string[] setup = File.ReadAllLines("setup.txt");
+                sourceDir = setup[0];
+                destDir = setup[1];
+                interval = int.Parse(setup[2]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
